e22c842d87cbfcb3c394049f1c162ae5b6fd817a
Ayush Raman <54257595+ayuram@users.noreply.github.com>
2024-05-01 14:09:58 -0500

    boiler code

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/translate', methods=['POST'])
+def translate_text():
+    text = request.form['text']
+    source = 'flutter'
+    target = 'react'
+    # TODO: Translate Text
+    translated_text = ""
+    return render_template('result.html', translated_text=translated_text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
