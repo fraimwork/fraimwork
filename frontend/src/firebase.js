55bd1a68df22befddb3b0ca3764d2878332775a3
Ayush Raman <ayushsraman@gmail.com>
2024-05-28 15:14:01 -0700

    analytics

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,10 +2,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions, httpsCallable } from "firebase/functions";
+import { getAnalytics } from "firebase/analytics";
 import firebaseConfig from "./api/firebaseConfig.json";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const functions = getFunctions(app);
+const analytics = getAnalytics(app);
 
 export { db, functions };
