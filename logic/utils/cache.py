0f18f9d799c07477f5f47da5d2e39345cde7e78c
Ayush Raman <54257595+ayuram@users.noreply.github.com>
2024-12-08 01:16:50 -0600

    more caching

--- logic/utils/cache.py
+++ logic/utils/cache.py
@@ -1,112 +1,244 @@
 from abc import ABC, abstractmethod
-import random
-from collections import defaultdict
-from queue import PriorityQueue
 import networkx as nx
-
+from collections import defaultdict, OrderedDict
 class Cache(ABC):
-    def __init__(self, capacity) -> None:
+    def __init__(self, capacity):
         self.capacity = capacity
-    # x in cache
+        self.graph = None
+        self.hits = 0
+        self.misses = 0
+    
     @abstractmethod
-    def __contains__(self, x):
+    def initialize(self, graph):
+        """Initialize the cache with a graph."""
+        self.graph = graph
+
+    @abstractmethod
+    def query(self, node):
+        """Query the cache with a node."""
         pass
 
-    # cache[x]
-    @abstractmethod
-    def __getitem__(self, x):
-        pass
+    def hit_rate(self):
+        """Calculate and return the hit rate."""
+        total = self.hits + self.misses
+        return self.hits / total if total > 0 else 0
 
-    @abstractmethod
-    def query(self, x):
-        pass
+from collections import OrderedDict
 
-class PageRankCache(Cache):
-    def __init__(self, capacity, graph) -> None:
+class LRUCache(Cache):
+    def __init__(self, capacity):
         super().__init__(capacity)
-        self.page_rank = nx.pagerank(graph)
-        # cache the top capacity nodes
-        self.cache = set(sorted(self.page_rank, key=self.page_rank.get, reverse=True)[:capacity])
+        self.cache = OrderedDict()
 
-    def __contains__(self, x):
-        return x in self.cache
+    def initialize(self, graph):
+        self.cache.clear()
+        super().initialize(graph)
+        page_rank = nx.pagerank(self.graph)
+        # cache the top capacity nodes by PageRank
+        for node, _ in sorted(page_rank.items(), key=lambda x: x[1], reverse=True)[:self.capacity]:
+            self.cache[node] = None
 
-    def __setitem__(self, x, y):
-        pass
-
-    def __getitem__(self, x):
-        return x in self.cache
+    def query(self, node):
+        if node in self.cache:
+            # Cache hit
+            self.hits += self.graph.nodes[node]['size']
+            # Move the accessed node to the end to show it was recently used
+            self.cache.move_to_end(node)
+            return self.cache[node]
+        else:
+            # Cache miss
+            self.misses += self.graph.nodes[node]['size']
+            if len(self.cache) >= self.capacity:
+                # Remove the least recently used item
+                self.cache.popitem(last=False)
+            # Add the new node to the cache
+            self.cache[node] = None
+            return self.cache[node]
 
 class LFUCache(Cache):
-    def __init__(self, capacity) -> None:
+    def __init__(self, capacity):
         super().__init__(capacity)
         self.cache = {}
         self.freq = defaultdict(int)
-        self.time = 0
-        self.pq = PriorityQueue()
-        self.min_freq = 0
+    
+    def initialize(self, graph):
+        self.cache.clear()
+        self.freq.clear()
+        super().initialize(graph)
+        page_rank = nx.pagerank(self.graph)
+        # cache the top capacity nodes by PageRank
+        for node, _ in sorted(page_rank.items(), key=lambda x: x[1], reverse=True)[:self.capacity]:
+            self.cache[node] = None
+            self.freq[node] = 0
 
-    def __contains__(self, x):
-        return x in self.cache
-
-    def __setitem__(self, x, y):
-        if self.capacity == 0:
-            return
-        if x in self.cache:
-            self.cache[x] = y
-            self.freq[x] += 1
-            self.pq.put((self.time, self.min_freq, x))
-            self.time += 1
+    def query(self, node):
+        if node in self.cache:
+            # Cache hit
+            self.hits += self.graph.nodes[node]['size']
+            # Increment the frequency of the node by 'size'
+            self.freq[node] += 1
+            return self.cache[node]
         else:
-            if len(self.cache) == self.capacity:
-                while self.pq:
-                    _, freq, node = self.pq.get()
-                    if self.freq[node] == freq:
-                        del self.cache[node]
-                        del self.freq[node]
-                        break
-            self.cache[x] = y
-            self.freq[x] = 1
-            self.pq.put((self.time, 1, x))
-            self.time += 1
-            self.min_freq = 1
-
-    def __getitem__(self, x):
-        if x in self.cache:
-            self.freq[x] += 1
-            self.pq.put((self.time, self.min_freq, x))
-            self.time += 1
-            return True
-        return False
-
-class LRUCache(Cache):
-    def __init__(self, capacity) -> None:
+            # Cache miss
+            self.misses += self.graph.nodes[node]['size']
+            if len(self.cache) >= self.capacity:
+                # Find the least frequently used node
+                lfu_node = min(self.freq, key=self.freq.get)
+                # Remove it from the cache and frequency dictionary
+                del self.cache[lfu_node]
+                del self.freq[lfu_node]
+            # Add the new node to the cache and set its frequency to 1
+            self.cache[node] = None
+            self.freq[node] = 1
+            return self.cache[node]
+class WeightedLFUCache(Cache):
+    def __init__(self, capacity):
         super().__init__(capacity)
         self.cache = {}
-        self.time = 0
-        self.pq = PriorityQueue()
+        self.freq = defaultdict(int)
+    
+    def initialize(self, graph):
+        self.cache.clear()
+        self.freq.clear()
+        super().initialize(graph)
+        page_rank = nx.pagerank(self.graph)
+        # cache the top capacity nodes by PageRank
+        for node, _ in sorted(page_rank.items(), key=lambda x: x[1], reverse=True)[:self.capacity]:
+            self.cache[node] = None
+            self.freq[node] = 0
 
-    def __contains__(self, x):
-        return x in self.cache
-
-    def __setitem__(self, x, y):
-        if self.capacity == 0:
-            return
-        if x in self.cache:
-            self.cache[x] = y
-            self.pq.put((self.time, x))
-            self.time += 1
+    def query(self, node):
+        if node in self.cache:
+            # Cache hit
+            self.hits += self.graph.nodes[node]['size']
+            # Increment the frequency of the node by 'size'
+            self.freq[node] += self.graph.nodes[node]['size']
+            return self.cache[node]
         else:
-            if len(self.cache) == self.capacity:
-                _, node = self.pq.get()
-                del self.cache[node]
-            self.cache[x] = y
-            self.pq.put((self.time, x))
-            self.time += 1
+            # Cache miss
+            self.misses += self.graph.nodes[node]['size']
+            if len(self.cache) >= self.capacity:
+                # Find the least frequently used node
+                lfu_node = min(self.freq, key=self.freq.get)
+                # Remove it from the cache and frequency dictionary
+                del self.cache[lfu_node]
+                del self.freq[lfu_node]
+            # Add the new node to the cache and set its frequency to 'size'
+            self.cache[node] = None
+            self.freq[node] = self.graph.nodes[node]['size']
+            return self.cache[node]
 
-    def __getitem__(self, x):
-        if x in self.cache:
-            self.pq.put((self.time, x))
-            self.time += 1
-            return True
-        return False
+class PageRankCache(Cache):
+    def __init__(self, capacity):
+        super().__init__(capacity)
+        self.cache = {}
+    
+    def initialize(self, graph):
+        self.cache.clear()
+        super().initialize(graph)
+        page_rank = nx.pagerank(self.graph)
+        # cache the top capacity nodes by PageRank
+        for node, _ in sorted(page_rank.items(), key=lambda x: x[1], reverse=True)[:self.capacity]:
+            self.cache[node] = None
+
+    def query(self, node):
+        if node in self.cache:
+            # Cache hit
+            self.hits += self.graph.nodes[node]['size']
+            return self.cache[node]
+        else:
+            # Cache miss
+            self.misses += self.graph.nodes[node]['size']
+            return None
+
+class WeightedPageRankCache(Cache):
+    def __init__(self, capacity):
+        super().__init__(capacity)
+        self.cache = {}
+    
+    def initialize(self, graph):
+        self.cache.clear()
+        super().initialize(graph)
+        personalization = {node: self.graph.nodes[node]['size'] for node in self.graph.nodes}
+        page_rank = nx.pagerank(self.graph, personalization=personalization)
+        # cache the top capacity nodes by PageRank
+        for node, _ in sorted(page_rank.items(), key=lambda x: x[1], reverse=True)[:self.capacity]:
+            self.cache[node] = None
+
+    def query(self, node):
+        if node in self.cache:
+            # Cache hit
+            self.hits += self.graph.nodes[node]['size']
+            return self.cache[node]
+        else:
+            # Cache miss
+            self.misses += self.graph.nodes[node]['size']
+            return None
+
+class InDegreeCache(Cache):
+    def __init__(self, capacity):
+        super().__init__(capacity)
+        self.cache = {}
+    
+    def initialize(self, graph):
+        self.cache.clear()
+        super().initialize(graph)
+        # cache the top capacity nodes by in-degree
+        for node, degree in sorted(self.graph.in_degree, key=lambda x: x[1], reverse=True)[:self.capacity]:
+            self.cache[node] = None
+
+    def query(self, node):
+        if node in self.cache:
+            # Cache hit
+            self.hits += self.graph.nodes[node]['size']
+            return self.cache[node]
+        else:
+            # Cache miss
+            self.misses += self.graph.nodes[node]['size']
+            return None
+
+class WeightedInDegreeCache(Cache):
+    def __init__(self, capacity):
+        super().__init__(capacity)
+        self.cache = {}
+    
+    def initialize(self, graph):
+        self.cache.clear()
+        super().initialize(graph)
+        inDegrees = dict(self.graph.in_degree)
+        weightedInDegrees = {node: inDegrees[node] * self.graph.nodes[node]['size'] for node in self.graph.nodes}
+        # cache the top capacity nodes by weighted in-degree
+        for node, degree in sorted(weightedInDegrees.items(), key=lambda x: x[1], reverse=True)[:self.capacity]:
+            self.cache[node] = None
+
+    def query(self, node):
+        if node in self.cache:
+            # Cache hit
+            self.hits += self.graph.nodes[node]['size']
+            return self.cache[node]
+        else:
+            # Cache miss
+            self.misses += self.graph.nodes[node]['size']
+            return None
+
+class TopKSizeCache(Cache):
+    def __init__(self, capacity):
+        super().__init__(capacity)
+        self.cache = {}
+    
+    def initialize(self, graph):
+        self.cache.clear()
+        super().initialize(graph)
+        # cache the top capacity nodes by size
+        for node, size in sorted(nx.get_node_attributes(self.graph, 'size').items(), key=lambda x: x[1], reverse=True)[:self.capacity]:
+            self.cache[node] = None
+
+    def query(self, node):
+        if node in self.cache:
+            # Cache hit
+            self.hits += self.graph.nodes[node]['size']
+            return self.cache[node]
+        else:
+            # Cache miss
+            self.misses += self.graph.nodes[node]['size']
+            return None
